5b93cec8a7f36837e12d5c0fdee25f220fcb91b1
Lacro59 <lacro59@gmail.com>
2020-11-27 14:40:18 +0100

    Minor updates

--- IndiegalaLibrary.cs
+++ IndiegalaLibrary.cs
@@ -47,7 +47,7 @@ namespace IndiegalaLibrary
             string pluginFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
             // Add plugin localization in application ressource.
-            PluginCommon.Localization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
+            PluginCommon.PluginLocalization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
             // Add common in application ressource.
             PluginCommon.Common.Load(pluginFolder);
 
