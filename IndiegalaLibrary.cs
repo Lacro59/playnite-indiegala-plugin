5514aba5790189be1ec22833acb336453054042a
Lacro59 <lacro59@gmail.com>
2020-12-24 14:38:32 +0100

    added common event

--- IndiegalaLibrary.cs
+++ IndiegalaLibrary.cs
@@ -46,6 +46,7 @@ namespace IndiegalaLibrary
             PluginCommon.PluginLocalization.SetPluginLanguage(pluginFolder, api.ApplicationSettings.Language);
             // Add common in application ressource.
             PluginCommon.Common.Load(pluginFolder);
+            PluginCommon.Common.SetEvent(PlayniteApi);
 
             // Check version
             if (settings.EnableCheckVersion)
